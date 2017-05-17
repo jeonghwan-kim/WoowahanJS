c84275b4761dd519bcca0b70a4895f465978c722
김정환 <ej88ej@gmail.com>
2017-05-17 10:43:19 +0900

    02-bind

---
+++ examples-copy/02-bind/src/hello-view.js
@@ -0,0 +1,20 @@
+import Woowahan from '../../../index'
+import Template from './hello-view.hbs'
+
+export default Woowahan.View.create('Hello', {
+  template: Template,
+
+  events: {
+    '@click #btn': 'onClick(#btn)'
+  },
+
+  initialize() {
+    this.setModel({msg: 'hello world!'});
+    this.super();
+  },
+
+  onClick(val) {
+    alert('val: ' + val)
+    console.log(val)
+  }
+})
